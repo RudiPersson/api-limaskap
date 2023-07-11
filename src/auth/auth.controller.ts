229c78175cf3af701245321698a0453e196ae9b6
Rudi <rudipersson@gmail.com>
2023-07-11 12:58:03 +0100

    feat: add dockerfile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { signInDto } from 'src/auth/dto/signIn.dto';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller({
   path: 'auth',
@@ -9,6 +10,7 @@ import { signInDto } from 'src/auth/dto/signIn.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: signInDto) {
