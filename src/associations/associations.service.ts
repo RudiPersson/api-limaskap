db7bec2222a4b8d75df8dcaa02856ced4a53df29
Rudi <rudipersson@gmail.com>
2023-07-13 15:56:45 +0100

    feat: add team signup to association

--- src/associations/associations.service.ts
+++ src/associations/associations.service.ts
@@ -35,15 +35,47 @@ export class AssociationsService {
     return association;
   }
 
-  findOneWhereUser(userId: string) {
-    const association = this.prisma.association.findUniqueOrThrow({
+  async findOneWhereUser(userId: string) {
+    const association = await this.prisma.association.findUniqueOrThrow({
       where: { userId: userId },
       include: {
-        teams: true,
+        teams: {
+          orderBy: {
+            updatedAt: 'desc',
+          },
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            email: true,
+            price: true,
+            member_size: true,
+            signup_deadline: true,
+            createdAt: true,
+            updatedAt: true,
+            associationId: true,
+            _count: {
+              select: {
+                member: true,
+              },
+            },
+          },
+        },
       },
     });
 
-    return association;
+    // Transform the _count to sign_ups
+    const teams = association.teams.map((team) => {
+      const { _count, ...rest } = team;
+      return { ...rest, sign_ups: _count ? _count.member : 0 };
+    });
+
+    const result = {
+      ...association,
+      teams,
+    };
+
+    return result;
   }
 
   update(id: string, updateAssociationDto: UpdateAssociationDto) {
